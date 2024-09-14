e4ccd6d21427147a6d98a729b578f72924dcbd75
malupadilla <145455169+malupadilla@users.noreply.github.com>
2024-09-14 18:39:58 -0300

    aaa

--- Src/javascript/script.js
+++ Src/javascript/script.js
@@ -1,1 +1,32 @@
-t
+const value = document.getElementById('value')
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')
+
+const updatValue = () => {
+    value.innerHTML = count;
+};
+
+let count = 0;
+let intervalid = 0;
+
+plusButton.addEventListener('mausedown', () => {
+    intervalid = setInterval(() => {
+        count +=1;
+        updatValue
+    }, 100);
+});
+
+minusButton.addEventListener('mausedown', () => {
+    intervalid = setInterval(() => {
+        count -=1;
+        updatValue();
+    }, 100);
+});
+
+resetButton.addEventListener('click', () => {
+    count = 0;
+    updatValue
+});
+
+document,addEventListener('mouseup', () => clearInterval(intervalid));
