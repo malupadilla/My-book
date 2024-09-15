f01260fc72e3bd651e30f7742232ea8cbb47ee32
malupadilla <145455169+malupadilla@users.noreply.github.com>
2024-09-15 17:37:37 -0300

    Atualizar o script.js
    yyyyyyy

--- Src/javascript/script.js
+++ Src/javascript/script.js
@@ -1,5 +1,3 @@
-let count = 0;
-
 // Verifica se existe um valor armazenado para 'count' no Local Storage
 if (localStorage.getItem('count')) {
     count = parseInt(localStorage.getItem('count'));
@@ -16,6 +14,8 @@ const updateValue = () => {
 };
 
 /* Clique Normal */
+
+let count = 0;
 
 plusButton.addEventListener('click', () => {
     count += 1;
